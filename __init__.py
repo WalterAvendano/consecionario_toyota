a283cfc9400dcd7a37ecfd955c913ff6b4fcbec9
Walter Avendano <walter.avendano.@gmail.com>
2025-09-24 12:56:37 -0400

    Primer commit - proyecto consecionario toyota

---
+++ __init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# Importando los módulos necesarios
+
+from . import reports
---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+# este archivo se encarga de definir la metadata y configuración principal de un modulo
+{
+    'name': 'Consecionario Toyota',
+    'version': '17.0.1.0.0',
+    'summary': 'Módulo para generar una factura, nota de credito y debito personalizadas con un duplicado para enviar por correo y su respectiva marca de agua ',
+    'author': 'Walter Avendano',
+    'website': 'https://www.tuweb.com',
+    'license': 'AGPL-3',
+    'category': 'Accounting/Localizations',
+    'depends': [
+        'account'
+    ],
+    'data': [
+        'reports/factura_toyota_reporte.xml',                       # creación de opcion de impresión de la factura
+        'reports/factura_toyota_template.xml',                      # creación de plantilla de la factura QWEB
+    ],
+    'assets': {
+        'web.report_assets_common': [
+            'consecionario_toyota/static/src/css/style.css',         	# archivo CSS para el estilo 
+        ],
+    },
+    'installable': True,
+    'application': False,
+}
